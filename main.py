398d28a3f253bedd865d5e2cd74b29bd69a42265
seshasai-1827 <seshasaichillara@gmail.com>
2025-05-14 15:29:29 +0530

    readme has the details of task

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
 
-#rules
-#dont change the version strings
-#2025 parameters supercede 2024
-#the values from 2024 need to replace the default values of the 2025 datamodel
-#if manage object has not gone to the next version then it doesnot need to be added as its depricated
-
-#phase 1
-#for implementation
-#develop a map of the file
-#perform crud operations
-
-#phase 2
-#if parameter names are changed...
-
-#parse through the old version file if tag is found in the new one then tranfer the parmeter values
-#for the second phse if the parameter names have been changed then edit the old versions parameter names then perform same operation 
-
-#TO DO
-#need to add initial 3 parts
-
 
 
 import xml.etree.ElementTree as ET
